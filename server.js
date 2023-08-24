9db16b288897f13a02c1762b6df2f90f053e376d
JoseCheo <joseche0.ja@gmail.com>
2023-08-23 23:44:05 -0500

    PORT

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const { ApolloServer } = require('apollo-server');
 const fs = require('fs');
 const path = require('path');
 const resolvers = require('./src/resolvers/resolvers');
-
+const PORT = process.env.PORT || 4000;
 const typeDefs = fs.readFileSync(path.join(__dirname, './src/schemas/schema.gql'), 'utf8');
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  PORT
 });
 
 server.listen().then(({ url }) => {
